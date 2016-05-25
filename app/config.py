ace6cf2c615776e9c03677bb8fec12c4889dd11d
Jake Bladt <jake@jakebladt.com>
2016-05-25 16:51:18 -0400

    use sqlalchemy

--- app/config.py
+++ app/config.py
@@ -1,2 +1,5 @@
 class Configuration(object):
+  APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
   DEBUG = True
+  SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/walker.db' % APPLICATION_DIR
+  
