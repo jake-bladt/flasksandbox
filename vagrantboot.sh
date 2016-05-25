6e2362c7dca30a322955ca1aae1f73aae1e7810f
Jake Bladt <jake@jakebladt.com>
2016-05-24 20:13:04 -0400

    more flask packages

--- vagrantboot.sh
+++ vagrantboot.sh
@@ -11,5 +11,7 @@ easy_install3 pip
 pip3.4 install pylint
 pip3.4 install pyflakes
 pip3.4 install flask
-
-
+pip3.4 install flask-wtf
+pip3.4 install flask-sqlalchemy
+pip3.4 install flask-login
+pip3.4 install flask-restless
