32204fe1a116a2c3d113e501e83338375b56aa38
Jake Bladt <jake@jakebladt.com>
2016-05-27 12:54:20 -0400

    ->Date

--- app/models.py
+++ app/models.py
@@ -4,7 +4,10 @@ from app import db
 class DailyReading(db.Model):
   id = db.Column(db.Integer,  primary_key=True)
   steps_count = db.Column(db.Integer)
-  reading_day = db.Column(db.Integer)
+  reading_day = db.Column(db.Date)
   notes = db.Column(db.String(255))
   created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
   modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+  def __repr__(self):
+    return '<%n steps on %d>' % (self.steps_count, self.reading_day)
