bb38c2ed32f1242444ce6fab12717ba2c9a320a9
Jake Bladt <jake@jakebladt.com>
2016-05-27 12:47:12 -0400

    +track mods

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ class Configuration(object):
   APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
   DEBUG = True
   SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/walker.db' % APPLICATION_DIR
+  SQLALCHEMY_TRACK_MODIFICATIONS = True
