91f34cb87127a05c8b4562895c5acd3ea7939596
Jake Bladt <jake@jakebladt.com>
2016-05-28 09:05:49 -0400

    get readings

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
+from flask import Blueprint
 from flask import render_template
+from helpers import object_list
+from models import DailyReading
 
 from app import app
 
 @app.route('/')
 def homepage():
-  return render_template('homepage.html')
+  readings = DailyReading.query.order_by(DailyReading.reading_day.desc())
+  return object_list('homepage.html', readings)
