4af410094bac9e430cedbbbf43bf01a3fcc4121f
Jake Bladt <jake@jakebladt.com>
2016-05-28 10:02:32 -0400

    +SECRET_KEY. Quelle horreur!

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@ import os
 class Configuration(object):
   APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
   DEBUG = True
+  SECRET_KEY = 'not really much of a secret'
   SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/walker.db' % APPLICATION_DIR
   SQLALCHEMY_TRACK_MODIFICATIONS = True
