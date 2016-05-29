7d9c994b3e55e02b77c949c3bb36a943a589af4b
Jake Bladt <jake@jakebladt.com>
2016-05-29 19:11:59 -0400

    +User

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from app import db
 class User(db.Model):
   id = db.Column(db.Integer,  primary_key=True)
   email = db.Column(db.String(128), unique=True)
+  password_hash = db.Column(db.String(255))
+  user_name = db.Column(db.String(128), unique=True)
+  is_active = db.Column(db.Boolean, default=True)
+  created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+  modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
 
 class DailyReading(db.Model):
   id = db.Column(db.Integer,  primary_key=True)
