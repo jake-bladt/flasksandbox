3a5a93c06edc0b93762aa17c78ad1832c10ccfe0
Jake Bladt <jake@jakebladt.com>
2016-05-29 12:10:53 -0400

    upsert

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for
 
-from app import app
+from app import app, db
 from models import DailyReading
 from forms import StepsDataForm
 from helpers import object_list
@@ -15,4 +15,6 @@ def stepsreading():
   form = StepsDataForm(request.form)
   if form.validate():
     new_reading = form.populate_reading(DailyReading())
-
+    existing_reading = session.query.filter(DailyReading.reading_day == new_reading.reading_day).first()
+  else
+    pass
