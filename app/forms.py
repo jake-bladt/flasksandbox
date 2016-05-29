cba8e2bc5a23b59abb83b144b21b877bf7c99c87
Jake Bladt <jake@jakebladt.com>
2016-05-29 14:09:43 -0400

    DataRequired->Required

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import Form
 from wtforms import DateField, IntegerField, validators
 
 class StepsDataForm(Form):
-  reading_day = DateField("Reading Date", [validators.DataRequired()], format='%m/%d/%y')
-  steps_count = IntegerField("Steps", [validators.DataRequired()])
+  reading_day = DateField("Reading Date", [validators.Required()], format='%m/%d/%y')
+  steps_count = IntegerField("Steps", [validators.Required()])
 
   def populate_reading(self, reading):
     self.populate_object(reading)
