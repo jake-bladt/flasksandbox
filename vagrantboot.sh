954c8b914645c6d23e4a114eb599bfb29db6bd64
Jake Bladt <jake@jakebladt.com>
2016-05-29 19:33:28 -0400

    include ssl/ffi

--- vagrantboot.sh
+++ vagrantboot.sh
@@ -6,7 +6,7 @@ apt-get -y install sqlite
 
 # install python tools
 apt-get -y install python3-setuptools
-apt-get -y install build-essential python3-dev
+apt-get -y install build-essential libssl-dev libffi-dev python3-dev
 
 #install pip
 easy_install3 pip
@@ -17,6 +17,7 @@ pip3.4 install flask-wtf
 pip3.4 install flask-sqlalchemy
 pip3.4 install flask-migrate
 pip3.4 install flask-login
+pip3.4 install flask-bcrypt
 pip3.4 install flask-restless
 
 # development packages
