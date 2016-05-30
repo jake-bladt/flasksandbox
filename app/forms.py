33a3b39d2a268e7deb3059a4dd9ad65f7dca1b9f
Jake Bladt <jake@jakebladt.com>
2016-05-29 22:48:59 -0400

    +validate

--- app/forms.py
+++ app/forms.py
@@ -14,3 +14,14 @@ class LoginForm(Form):
   email = wtforms.StringField("Email", validators=[validators.Required()])
   password = wtforms.PasswordField("Password", validators=[validators.Required()])
   remember_me = wtforms.BooleanField("Remember me?", default=True)
+
+  def validate(self):
+    if not super(LoginForm, self).validate():
+      return False
+
+    self.user = User.authenticate(self.email.data, self.password.data)
+    if not self.user:
+      self.email.errors.append("Invalid email or password.")
+      return False
+
+    return True
