294acea2e963d28436cf162613182695290c9124
Jake Bladt <jake@jakebladt.com>
2016-05-29 22:53:01 -0400

    +login view

--- app/views.py
+++ app/views.py
@@ -23,3 +23,16 @@ def stepsreading():
     db.session.commit()
 
   return redirect(url_for('homepage'))
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+  if request.method == "POST":
+    form = LoginForm(request.form)
+    if form.validate():
+      login_user(form.user, remember=form.remember_me.data)
+      flash("Successfully logged in as %s." % form.user.email, "success")
+      return redirect(request.args.get("next") or url_for("homepage"))
+    else:
+      form = LoginForm()
+
+    return render_template("login.html", form=form)
