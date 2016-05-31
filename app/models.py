9bbd0575d47dfd2ede3a0c40dbf63ff3ead4e2a7
Jake Bladt <jake@jakebladt.com>
2016-05-31 09:52:31 -0400

    key to user id

--- app/models.py
+++ app/models.py
@@ -49,6 +49,7 @@ def _load_user(user_id):
 
 class DailyReading(db.Model):
   id = db.Column(db.Integer,  primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
   steps_count = db.Column(db.Integer)
   reading_day = db.Column(db.Date)
   notes = db.Column(db.String(255))
