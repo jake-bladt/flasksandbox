964aaf2137356f9e4f53d262c07fa796eb0dc943
Jake Bladt <jake@jakebladt.com>
2016-05-31 09:33:39 -0400

    +query

--- app/models.py
+++ app/models.py
@@ -44,8 +44,8 @@ class User(db.Model):
     return False
 
 @login_manager.user_loader
-def load_user(user_id):
-  return User.get(user_id)
+def _load_user(user_id):
+  return User.query.get(int(user_id))
 
 class DailyReading(db.Model):
   id = db.Column(db.Integer,  primary_key=True)
